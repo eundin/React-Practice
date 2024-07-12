ad007afd611a51360f804b5b5b5ae01c87489373
eundin <escience1202@gmail.com>
2024-07-12 15:48:35 +0900

    5-3

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,3 +1,4 @@
+import "./Main.css";
 //JSX 주의 사항
 //1. 중괄호 내부에는 js  표현식만 넣을 수 있다.
 //2. 숫자, 문자열, 배열 값만 렌더링 된다.
@@ -5,18 +6,15 @@
 //4. 최상위 태그는 반드시 하나여야만 한다.
 
 export default function Main() {
-  const number = 9;
-  const obj = { a: 1 };
-  return (
-    <main>
-      <h1>main</h1>
-      <h2>{number % 2 === 0 ? "짝수" : "홀수"}</h2>
-      {10}
-      {number}
-      {undefined}
-      {true}
-      {null}
-      {obj.a}
-    </main>
-  );
+  const user = {
+    name: "이정환",
+    isLogin: true,
+  };
+
+  if (user.isLogin) {
+    return <div className="logout">로그아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
+  // return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
 }
