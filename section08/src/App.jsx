415231b34eb7cb559c32dd493ede718d598822ca
eundin <escience1202@gmail.com>
2024-07-16 13:17:41 +0900

    07.16: 8-3

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
-
+const mock = [
+  {
+    isDone: false,
+    content: "react 공부하기",
+    id: 0,
+    date: new Date().getTime(),
+  },
+  {
+    isDone: false,
+    content: "wynn",
+    id: 1,
+    date: new Date().getTime(),
+  },
+  {
+    isDone: false,
+    content: "din",
+    id: 2,
+    date: new Date().getTime(),
+  },
+];
 function App() {
+  const [todos, setTodos] = useState(mock);
+
   return (
     <div className="App">
       <Header />
