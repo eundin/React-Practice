0fe02def0ee984be15244308eb89542c95410006
eundin <escience1202@gmail.com>
2024-07-24 21:33:06 +0900

    07.24: 12-8

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,48 +4,45 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
-import { getEmotionImages } from "./util/get-emotion-image";
-import Button from "./components/Button";
-import Header from "./components/Header";
 
-//1. "/": 모든 일기를 조회하는 Home 페이지
-//2. "/new": 새로운 일기를 작성하는 New 페이지
-//3. "/diary": 일기를 상세히 조회하는 Diary 페이지
+import Edit from "./pages/Edit";
+import { useReducer } from "react";
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date().getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
+  const [state, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"left"} />}
-        rightChild={<Button text={"right"} />}
-      />
-      <Button
-        text={"123"}
-        onClick={() => console.log("click")}
-        type={"DEFAULT"}
-      />
-      <Button
-        text={"123"}
-        onClick={() => console.log("click")}
-        type={"POSITIVE"}
-      />
-      <Button
-        text={"123"}
-        onClick={() => console.log("click")}
-        type={"NEGATIVE"}
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
